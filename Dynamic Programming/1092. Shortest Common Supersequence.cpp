56478fd6adac8a2335c328e3814f6c536d44cfff
Rajan-226 <56467449+Rajan-226@users.noreply.github.com>
2020-10-19 13:32:20 +0530

    Create 1092. Shortest Common Supersequence.cpp

---
+++ Dynamic Programming/1092. Shortest Common Supersequence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n=str1.size(),m=str2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1])  dp[i][j]=dp[i-1][j-1]+1;
+                else    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        int i=n,j=m;
+        string ans="";
+        while(i>=1&&j>=1){
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
+                i--;j--;
+            }
+            else if(dp[i][j]==dp[i][j-1]){
+                ans+=str2[j-1];
+                j--;
+            }
+            else{
+                ans+=str1[i-1];
+                i--;
+            }
+        }
+        while(i>=1){
+            ans+=str1[i-1];
+            i--;
+        }
+        while(j>=1){
+            ans+=str2[j-1];
+            j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
